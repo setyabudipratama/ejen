62ca777a007ca321b04f322c581109146ec23d37
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-20 19:59:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ const aBaru1 = document.createElement('a');
 const teksABaru1 = document.createTextNode('Ketua Pilar');
 // masukkan teks
 aBaru1.appendChild(teksABaru1);
-// tambah a
+// tambah tag a ke parent
 navItemBaru1.appendChild(aBaru1);
 // tambah kelas
 aBaru1.setAttribute('class', 'nav-link');
@@ -77,4 +77,76 @@ navItemBaru1.style.cursor = 'pointer';
 navItemBaru1.addEventListener('click', () => {
     const ketua = document.getElementById('ketua');
     ketua.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// buat li baru ke dua
+const navItemBaru2 = document.createElement('li');
+// masukkan ke parent dan sibling paling akhir
+navbarNav.insertBefore(navItemBaru2, liAkhir);
+// tambah atribut
+navItemBaru2.setAttribute('class', 'nav-item');
+// buat a baru sebagai child
+const aBaru2 = document.createElement('a');
+// buat teks
+const textBaru2 = document.createTextNode('Top Ejen');
+// masukkan teks
+aBaru2.appendChild(textBaru2);
+// tambah tag a ke parent
+navItemBaru2.appendChild(aBaru2);
+// tambah kelas
+aBaru2.setAttribute('class', 'nav-link');
+navItemBaru2.style.cursor = 'pointer';
+// fungsi untuk scroll ke top ejen
+navItemBaru2.addEventListener('click', () => {
+    const top = document.getElementById('top');
+    top.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// buat li baru ketiga
+const navItemBaru3 = document.createElement('li');
+// masukkan ke parent dan sibling paling akhir
+navbarNav.insertBefore(navItemBaru3, liAkhir);
+// tambah atribut
+navItemBaru3.setAttribute('class', 'nav-item');
+// buat a baru
+const aBaru3 = document.createElement('a');
+// buat teks
+const textBaru3 = document.createTextNode('Mentor');
+// masukkan teks
+aBaru3.appendChild(textBaru3);
+// masukkan tag a ke parent
+navItemBaru3.appendChild(aBaru3);
+// tambah kelas
+aBaru3.setAttribute('class', 'nav-link');
+navItemBaru3.style.cursor = 'pointer';
+// fungsi untuk scroll ke mentor
+navItemBaru3.addEventListener('click', () => {
+    const mentor = document.getElementById('mentor');
+    mentor.scrollIntoView({ behavior: 'smooth' });
+});
+
+
+// buat li baru keempat
+const navItemBaru4 = document.createElement('li');
+// masukkan ke parent dan sibling paling akhir
+navbarNav.insertBefore(navItemBaru4, liAkhir);
+// tambah atribut class
+navItemBaru4.setAttribute('class', 'nav-item');
+// buat a baru sebagai sibling
+const aBaru4 = document.createElement('a');
+// buat teks
+const textBaru4 = document.createTextNode('Top Ejen in Akademi');
+// masukkan teks
+aBaru4.appendChild(textBaru4);
+// masukkan tag a ke parent
+navItemBaru4.appendChild(aBaru4);
+// tambah attribute class
+aBaru4.setAttribute('class', 'nav-link');
+navItemBaru4.style.cursor = 'pointer';
+// fungsi untuk scroll ke top ejen in akademi
+navItemBaru4.addEventListener('click', () => {
+    const topEjen = document.getElementById('ejen');
+    topEjen.scrollIntoView({ behavior: 'smooth' });
 });
