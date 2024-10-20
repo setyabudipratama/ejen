4297bfb6378adc9370e88e802cdd4fdfc5e8deb3
Setya Budi Pratama <setyabudipratam@gmail.com>
2024-10-20 19:04:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,3 +42,39 @@ const home = document.querySelector('.nav-item:first-child .nav-link');
 home.addEventListener('click', () => {
     home.scrollIntoView({top: 0, behavior: 'smooth' });
 });
+
+
+// tambah class
+// ambil parent
+const navbarCollapse = document.querySelector('.navbar .navbar-collapse');
+// tambah kelas
+navbarCollapse.classList.add('justify-content-end');
+
+
+// tambah nav-item
+// buat nav-item
+const navItemBaru1 = document.createElement('li');
+// ambil parent
+const navbarNav = document.querySelector('.navbar .navbar-nav');
+// ambil child paling akhir
+const liAkhir = document.querySelector('.navbar .nav-item:last-child');
+// tambah nav-item
+navbarNav.insertBefore(navItemBaru1, liAkhir);
+// tambah atribut
+navItemBaru1.setAttribute('class', 'nav-item');
+// buat a baru
+const aBaru1 = document.createElement('a');
+// buat teks
+const teksABaru1 = document.createTextNode('Ketua Pilar');
+// masukkan teks
+aBaru1.appendChild(teksABaru1);
+// tambah a
+navItemBaru1.appendChild(aBaru1);
+// tambah kelas
+aBaru1.setAttribute('class', 'nav-link');
+navItemBaru1.style.cursor = 'pointer';
+
+navItemBaru1.addEventListener('click', () => {
+    const ketua = document.getElementById('ketua');
+    ketua.scrollIntoView({ behavior: 'smooth' });
+});
